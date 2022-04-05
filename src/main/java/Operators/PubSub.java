0132a45c42f11311a87084f3761374bc29999fe0
jinhyeong <jinhyeong.b@gmail.com>
2022-04-06 00:17:24 +0900

    Extract superclass

--- src/main/java/Operators/PubSub.java
+++ src/main/java/Operators/PubSub.java
@@ -21,14 +21,14 @@ import static java.util.stream.Collectors.toList;
  * Publisher 프로바이더 역할 데이터를 계속 생산
  * <p>
  * Publisher -> Data1 -> Operator1(가공) -> [Data2] -> Operator2 -> [Data3] -> Subscriber
- *
+ * <p>
  * 1. map(d1 -> function -> d2)
  * Publisher -> Data1 -> mapPub -> [Data2] -> logSub
- *                       <- subscribe(logSub)
- *                       -> onSubscribe(s)
- *                       -> onNext
- *                       -> onNext
- *                       -> onComplete
+ * <- subscribe(logSub)
+ * -> onSubscribe(s)
+ * -> onNext
+ * -> onNext
+ * -> onComplete
  */
 @Slf4j
 public class PubSub {
@@ -36,37 +36,26 @@ public class PubSub {
 
 		// 데이터를 보내는거
 		Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a + 1).limit(10).collect(toList()));
-		Publisher<Integer> mapPub = mapPub(pub, (Function<Integer, Integer>)s -> s * 10 );
+//		Publisher<Integer> mapPub = mapPub(pub, (Function<Integer, Integer>) s -> s * 10);
+//		Publisher<Integer> map2Pub = mapPub(mapPub, (Function<Integer, Integer>) s -> -s);
+		Publisher<Integer> sumPub = sumPub(pub);
 
 		// 퍼블리셔 부터 4개 메서드 종류의 데이터를 받는다
 		Subscriber<Integer> sub = logSub();
 
-		mapPub.subscribe(sub);
+		sumPub.subscribe(sub);
 	}
+
+
 
 	private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> f) {
 		return new Publisher<Integer>() {
 			@Override
 			public void subscribe(Subscriber<? super Integer> sub) {
-				pub.subscribe(new Subscriber<Integer>() {
-					@Override
-					public void onSubscribe(Subscription s) {
-						sub.onSubscribe(s);
-					}
-
+				pub.subscribe(new DelegateSub(sub) {
 					@Override
 					public void onNext(Integer integer) {
 						sub.onNext(f.apply(integer));
-					}
-
-					@Override
-					public void onError(Throwable t) {
-						sub.onError(t);
-					}
-
-					@Override
-					public void onComplete() {
-						sub.onComplete();
 					}
 				});
 			}
